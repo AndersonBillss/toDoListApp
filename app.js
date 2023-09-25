fdbca248b29cd54867e1724a6c0ffc1783e28424
AndersonBillss <anderson@thebillsfamily.com>
2023-09-25 08:26:14 -0600

    made render function

--- app.js
+++ app.js
@@ -9,12 +9,21 @@ document.getElementById('newListButton').addEventListener('click',function(){
     if (newListTitle != 0 && newListTitle != null && newListTitle !=undefined ){
         lists[listNumber]=({name:newListTitle, todos: []})
         listNumber++
+        render()
     }
     console.log(lists)
 }
 )
 
 
+
 function render(){
-    
+    let listsHtml = `<ul class="listGroup">`;
+    for(i=1; i<=Object.keys(lists).length; i++){
+        let listItem = lists[i];
+        console.log(listItem.name)
+        listsHtml += `<li class="listGroupItem">` + listItem.name + `</li>`;
+    }
+      listsHtml += `</ul>`
+      document.getElementById("lists").innerHTML = listsHtml;
 }
